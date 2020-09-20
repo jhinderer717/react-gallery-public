457db15bbd29af7da52c4a2b87d74fda3dd78818
John Hinderer <john.hinderer16@gmail.com>
2020-09-19 21:51:42 -0500

    description toggle works

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -3,23 +3,50 @@ import React, { Component } from 'react';
 
 class GalleryItem extends Component {
 
-    // bop = () => {
-    //     console.log('bopped');
-    //     this.props.pic.likes ++;
-    //     console.log('likes:', this.props.pic.likes);
-    //     this.render();
-    // }
+    state = {
+        showPic: true
+    }
+
+    handleClick = () => {
+        console.log('in handleClick');
+        this.setState({
+            showPic: !this.state.showPic
+        });
+    } // end handleClick
 
     render() {
         console.log('props from GalleryItem:', this.props);
         console.log('this.props.pic.id', this.props.pic.id);
         return (
             <div className="GalleryItem">
-                <img src={ this.props.pic.path } height="150"/>
+                <div onClick={this.handleClick}>
+                    {
+                        this.state.showPic ?
+                            <img src={ this.props.pic.path } height="150" alt=""/> :
+                            <p>{ this.props.pic.description }</p>
+                    }
+                </div>
                 <button onClick={() => this.props.onLike(this.props.pic.id)}>B for Bop!</button>
-                <p className="likes">{this.props.pic.likes} people bopped this</p>
+                <p>{this.props.pic.likes} people bopped this</p>
             </div>
         );
+        // if( !this.state.showDescription ){
+        //     return (
+        //         <div className="GalleryItem">
+        //             <div onClick={this.handleClick}><img src={ this.props.pic.path } height="150" alt=""/></div>
+        //             <button onClick={() => this.props.onLike(this.props.pic.id)}>B for Bop!</button>
+        //             <p>{this.props.pic.likes} people bopped this</p>
+        //         </div>
+        //     );
+        // }else{
+        //     return(
+        //         <div className="GalleryItem">
+        //             <div onClick={this.handleClick}>{ this.props.pic.description }</div>
+        //             <button onClick={() => this.props.onLike(this.props.pic.id)}>B for Bop!</button>
+        //             <p>{this.props.pic.likes} people bopped this</p>
+        //         </div>
+        //     );
+        // }
     }
 }
 
